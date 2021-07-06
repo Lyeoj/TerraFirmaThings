b6d3596abc4d29fda3b68d2412e141d43404a06d
Lyeoj <joekuel@frontier.com>
2021-07-06 11:10:48 -0400

    Fix prospector's hammer to use new falling block manager

--- src/main/java/lyeoj/tfcthings/items/ItemProspectorsHammer.java
+++ src/main/java/lyeoj/tfcthings/items/ItemProspectorsHammer.java
@@ -12,11 +12,11 @@ import net.dries007.tfc.api.capability.size.Size;
 import net.dries007.tfc.api.capability.size.Weight;
 import net.dries007.tfc.api.types.Metal;
 import net.dries007.tfc.api.types.Rock;
+import net.dries007.tfc.api.util.FallingBlockManager;
 import net.dries007.tfc.objects.blocks.stone.BlockRockVariant;
+import net.dries007.tfc.objects.blocks.stone.BlockRockVariantFallable;
 import net.dries007.tfc.objects.blocks.wood.BlockSupport;
 import net.dries007.tfc.objects.items.ItemTFC;
-import net.dries007.tfc.util.ICollapsableBlock;
-import net.dries007.tfc.util.IFallingBlock;
 import net.dries007.tfc.util.skills.ProspectingSkill;
 import net.dries007.tfc.util.skills.SkillType;
 import net.dries007.tfc.util.skills.SmithingSkill;
@@ -123,8 +123,8 @@ public class ItemProspectorsHammer extends ItemTFC implements IMetalItem, ItemOr
                     return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
                 }
                 int messageType = 0;
-                if(block instanceof ICollapsableBlock) {
-                    boolean result = isThisBlockSafe(worldIn, blockpos);
+                if(FallingBlockManager.getSpecification(worldIn.getBlockState(blockpos)) != null && FallingBlockManager.getSpecification(worldIn.getBlockState(blockpos)).isCollapsable()) {
+                    boolean result = isThisBlockSafe(worldIn, blockpos);;
                     float falsePositiveChance = 0.3F;
                     if (skill != null) {
                         falsePositiveChance = 0.3F - 0.1F * (float)skill.getTier().ordinal();
@@ -176,16 +176,15 @@ public class ItemProspectorsHammer extends ItemTFC implements IMetalItem, ItemOr
     }
 
     private boolean isThisBlockSafe(World worldIn, BlockPos pos) {
-        int radX = (Constants.RNG.nextInt(5) + 4) / 2;
-        int radY = (Constants.RNG.nextInt(3) + 2) / 2;
-        int radZ = (Constants.RNG.nextInt(5) + 4) / 2;
+        int radX = 4;
+        int radY = 2;
+        int radZ = 4;
         Iterator var6 = BlockSupport.getAllUnsupportedBlocksIn(worldIn, pos.add(-radX, -radY, -radZ), pos.add(radX, radY, radZ)).iterator();
 
         while(var6.hasNext()) {
             BlockPos checking = (BlockPos)var6.next();
-            if (worldIn.getBlockState(checking).getBlock() instanceof ICollapsableBlock) {
-                ICollapsableBlock block = (ICollapsableBlock)worldIn.getBlockState(checking).getBlock();
-                if (block.canCollapse(worldIn, checking)) {
+            if (FallingBlockManager.getSpecification(worldIn.getBlockState(checking)) != null && FallingBlockManager.getSpecification(worldIn.getBlockState(checking)).isCollapsable()) {
+                if (FallingBlockManager.canCollapse(worldIn, checking)) {
                     return false;
                 }
             }
@@ -196,7 +195,7 @@ public class ItemProspectorsHammer extends ItemTFC implements IMetalItem, ItemOr
     private boolean supportingFallable(World worldIn, BlockPos pos) {
         IBlockState iblockstate = worldIn.getBlockState(pos.up());
         Block block = iblockstate.getBlock();
-        if(block instanceof IFallingBlock || block instanceof BlockFalling) {
+        if(block instanceof BlockRockVariantFallable || block instanceof BlockFalling) {
             return !BlockSupport.isBeingSupported(worldIn, pos.up());
         }
         return false;
